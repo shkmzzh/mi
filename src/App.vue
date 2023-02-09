e25b2d175ca73467772dc848b67221292e67dfe5
shkmzzh <3081764857@qq.com>
2023-02-09 23:16:53 +0800

    add banner

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <div id="app">
     <Nav></Nav>
+    <Banner></Banner>
   </div>
 </template>
 
 <script>
 import '@/styles/base.css'
 import Nav from '@/components/nav/index.vue'
+import Banner from '@/components/banner/index.vue'
 export default {
   name: 'App',
   components: {
-    Nav
+    Nav,
+    Banner
   }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
---
+++ src/components/banner/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="banner w">
+    <div class="sidebar"></div>
+    <div class="ration">
+      <el-carousel trigger="click" height="150px">
+        <el-carousel-item v-for="item in 4" :key="item">
+          <h3 class="small">{{ item }}</h3>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
